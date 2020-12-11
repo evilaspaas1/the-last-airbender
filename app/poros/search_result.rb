dd3b9a9cf68ec4cca0ae6597126e964b298f6701
Austin Aspaas <araspaas@live.com>
2020-12-11 10:55:18 -0700

    Edit search result poro to get the info for members

--- app/poros/search_result.rb
+++ app/poros/search_result.rb
@@ -1,9 +1,25 @@
 class SearchResult
-  def initialize(nation)
+  attr_reader :name,
+              :nation
 
+  def initialize(nation_name)
+    @name = format_name(nation_name)
+    @nation = format_nation(nation_name)
+  end
+
+  def format_name(name)
+    name.split("_")[1]
+  end
+
+  def format_nation(nation)
+    nation.split("_")[0]
   end
 
   def members
-
+    las = LastAirbenderService.new
+    members = las.members(@nation, @name)
+    members.map do|member|
+      Member.new(member)
+    end
   end
 end
