779d9cb383c1bbf60496f4ce274aeebb5cc9f33c
Austin Aspaas <araspaas@live.com>
2020-12-11 10:01:41 -0700

    Add tests for search result poro

---
+++ spec/poros/search_results_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "SearchResult poro" do
+  it "exists" do
+    search_result = SearchResult.new("Fire Nation")
+
+    expect(search_result).to be_instance_of(SearchResult)
+  end
+
+  it "has attributes" do
+    search_result = SearchResult.new("Fire Nation")
+
+    expect(search_result.nation).to eq("Fire Nation")
+  end
+
+  context "instance methods" do
+    it "#members" do
+      search_result = SearchResult.new("Fire Nation")
+
+      expect(search_result.members.count).to eq(20)
+      expect(search_result.members).to be_an(Array)
+    end
+  end
+end
